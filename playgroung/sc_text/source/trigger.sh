30c68bbf0228251b008a6397f91da89058f7333b
Robert-Rino <a901002666@gmail.com>
2016-03-26 22:48:00 +0800

    add shell script

---
+++ playgroung/sc_text/source/trigger.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python3 /Users/robert/Repository/scViewTest/playgroung/sc_text/source/generateHotKeyword.py
+# python sc_text/source/preprocess_weeks_data.py
+# python3 sc_text/source/subtitlePreprocess.py
+# python3 generateHotKeyword.py
